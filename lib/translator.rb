d16eab42b7240e7c31d112f656d3ebbfdc084e8d
Sohyun Lee <sohyunc1990@gmail.com>
2020-07-07 15:54:17 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,18 @@
 # require modules here
+require 'pry'
+require 'yaml'
 
 def load_library
+  emoticons = YAML.load_file('./lib/emoticons.yml')
   # code goes here
+    meaning_emoticon = {
+      meaning={},
+      emoticon={}
+    }
+    emoticons.each do |meaning, emoticon|
+      meaning_emoticon[:meaning] << meaning
+      meaning_emoticon[:emoticon] << emoticon
+ binding.pry
 end
 
 def get_japanese_emoticon
